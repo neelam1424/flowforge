b1ee71deb9578ff7a3a91bb847bf59c9069cca23
neelam1424 <neelammore1404@gmail.com>
2026-02-04 23:58:19 -0500

    12:Node Variables

--- src/features/executions/components/http-request/executor.ts
+++ src/features/executions/components/http-request/executor.ts
@@ -4,6 +4,7 @@ import ky, { type Options as KyOptions} from "ky"
 
 
 type HttpRequestData = {
+    variableName?: string;
     endpoint?: string;
     method?: "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
     body?: string;
@@ -26,6 +27,10 @@ export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async({
         throw new NonRetriableError("HTTP Request node: No endpoint configured");
     }
 
+    if(!data.variableName){
+        throw new NonRetriableError("Variable name not configured");
+    }
+
 
     const result = await step.run("http-request", async ()=>{
         const endpoint = data.endpoint!;
@@ -35,6 +40,10 @@ export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async({
 
         if(["POST", "PUT" ,"PATCH"].includes(method)){
                 options.body = data.body;
+                options.headers = {
+                    "Content-Type": "application/json",
+
+                }
         }
 
         const response = await ky(endpoint, options);
@@ -44,14 +53,27 @@ export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async({
         : await response.text();
 
 
-        return{
-            ...context,
+        const responsePayload = {
             httpResponse:{
                 status: response.status,
                 statusText: response.statusText,
                 data: responseData,
             }
         }
+
+
+        if (data.variableName){
+        return{
+            ...context,
+            [data.variableName]: responsePayload,
+        }
+    }
+
+    //Fallback to direct httpResponse for backward compatibility
+    return{
+        ...context,
+        ...responsePayload,
+    }
     })
 
 
